0f0cd174c1574ad6061570bc285c965b9d6eced9
Carlos Evelo <carlosevelo96@gmail.com>
2023-09-28 19:18:55 -0600

    Initial Commit

---
+++ TimeLogger/TimeLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLoggerApp.swift
+//  TimeLogger
+//
+//  Created by Carlos Evelo on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
